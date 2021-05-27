b53e1973c773dc8837ee5f64474d2f57a80c4918
Marie G <marie.goeske@yahoo.de>
2021-05-27 15:36:49 +0200

    finished game

---
+++ main.js
@@ -0,0 +1,38 @@
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    let winner;
+    // look at selection of player and compare to selection of computer
+    if(playerSelection === "rock" && computerSelection !== "paper"){
+     winner = true;
+    }
+    else if(playerSelection === "paper" && computerSelection !== "scissor"){
+        winner = true;
+       }
+    else if(playerSelection === "scissor" && computerSelection !== "rock"){
+        winner = true;
+       }
+
+       
+    return (winner ? 'player wins!':'computer wins!');
+ }
+
+ function game(){
+    let playerSelection;
+    let computerSelection;
+     for(let i = 0; i< 5; i++){
+       computerSelection = computerChooses();
+       console.log(computerSelection);
+       playerSelection = prompt("Choose rock paper or scissor!");
+       console.log(playerSelection);
+       if(playerSelection === null){
+           console.log("Du hast nichts eingeben",null)
+       }else{
+        console.log(playRound(playerSelection,computerSelection));
+       }
+     }
+ }
+
+ function computerChooses(){
+    let selections = Array("rock","paper","scissor")
+    return selections[Math.floor(Math.random() * selections.length)];
+ }
